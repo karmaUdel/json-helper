289e35a34ba63cb3f463d464fe0d60c1c7ec27e4
karmaUdel <karma@udel.edu>
2021-06-06 13:50:02 -0700

    adding initial repositiory creation commit

---
+++ starter/json-helper-starter/src/test/java/com/open/source/json/helper/starter/AppTest.java
@@ -0,0 +1,18 @@
+package com.open.source.json.helper.starter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
